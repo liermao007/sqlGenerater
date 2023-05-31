304b574c1ad3a8da493aeba997d398ab5fc95ab7
lgx <690101606@qq.com>
2023-05-31 14:33:38 +0800

    测试

--- src/main/java/org/xian/Main.java
+++ src/main/java/org/xian/Main.java
@@ -1,21 +1,30 @@
 package org.xian;
 
+import cn.hutool.db.Entity;
+import cn.hutool.db.sql.SqlBuilder;
+import org.xian.utils.SQLExecutor;
+
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * @author lgx
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String dataJson = """
                 {
                     params: {
-                        title: '1',
-                        name: '23'
+                        su1: 'admin',
                     },
-                    results: ['title', 'name']
+                    results: ['su1', 'su3']
                 }
                 """;
-        String sql = SqlGenerater.createSelect(dataJson).sql();
-
+        SqlBuilder sql = SqlGenerator.select(dataJson).sqlBuilder();
+        System.out.println(sql);
+        long l = System.currentTimeMillis();
+        List<Entity> entities = SQLExecutor.queryList(Entity.class, sql);
+        System.out.println(System.currentTimeMillis() - l);
         System.out.println(sql);
     }
 }
