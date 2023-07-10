3128ad355ab2258d513129db262f02f16fb10de6
lgx <690101606@qq.com>
2023-07-10 14:30:11 +0800

    update

---
+++ generator/src/main/java/com/xian/controller/TableController.java
@@ -0,0 +1,14 @@
+package com.xian.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author lgx
+ */
+@Controller
+@RequestMapping("table")
+public class TableController {
+
+
+}
