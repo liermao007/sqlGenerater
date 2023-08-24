cbc1b245ceafd5b8fe12963b6750581f1ee4f6bf
lgx <690101606@qq.com>
2023-08-24 14:03:55 +0800

    update bug

--- generator/src/main/java/com/xian/mapper/ConfigTableMapper.java
+++ generator/src/main/java/com/xian/mapper/ConfigTableMapper.java
@@ -2,7 +2,6 @@ package com.xian.mapper;
 
 import com.xian.domain.ConfigTable;
 import com.xian.domain.vo.ConfigTableVo;
-import org.dromara.common.mybatis.core.mapper.BaseMapperPlus;
 
 /**
  * 数据库相关表 数据层
